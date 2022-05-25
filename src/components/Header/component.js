2da38b674e257880d208ecb89d07883be0c1d562
Sandro Herrera Pallares <sandrohp88@gmail.com>
2022-05-25 11:04:31 -0700

    Update Header component

--- src/components/Header/component.js
+++ src/components/Header/component.js
@@ -1,8 +1,14 @@
 import styles from "./styles.module.css";
+import food_img from "../../assets/meals.jpg";
 export const Header = (props) => {
   return (
-    <div className={styles["header"]}>
-      <h1>Food App</h1>
-    </div>
+    <>
+      <header className={styles.header}>
+        <h1>Food App</h1>
+      </header>
+      <div className={styles["main-image"]}>
+        <img src={food_img} alt="Table of food" />
+      </div>
+    </>
   );
 };
