fa50de64e977cbab1f2571c3a97c775e45a310f1
Sandro Herrera Pallares <sandrohp88@gmail.com>
2022-05-29 12:02:32 -0700

    Pass context down in the component tree

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,24 @@
 import Header from "../Header";
+import Meals from "../Meals";
+import CartProvider from "../../store/CartProvider";
+import Cart from "../Cart";
+import { useState } from "react";
 const App = () => {
+  const [isCartShown, setIsCartShown] = useState(false);
+  const showCartHandler = () => {
+    setIsCartShown(true);
+  };
+  const hideCartHandler = () => {
+    setIsCartShown(false);
+  };
   return (
-    <>
-      <Header></Header>
-    </>
+    <CartProvider>
+      {isCartShown && <Cart onCloseClicked={hideCartHandler} />}
+      <Header onCartClicked={showCartHandler}></Header>
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
   );
 };
 
